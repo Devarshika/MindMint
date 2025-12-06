5422cfeb402dd7b0866df4bed6150e92f8af3bdc
Singuluri Devarshika <147074041+Devarshika@users.noreply.github.com>
2025-12-06 17:01:56 +0530

    Update mindmint_app.py

--- mindmint_app.py
+++ mindmint_app.py
@@ -3,7 +3,7 @@ import PyPDF2
 import docx2txt
 from openai import OpenAI
 import os
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+openai.api_key = os.getenv("OPENAI_API_KEY")
 st.set_page_config(page_title="MindMint | Smart Summarizer", page_icon="🧠")
 
 st.title("🧠 MindMint – Smart AI Summarizer")
